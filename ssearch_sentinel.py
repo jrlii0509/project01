a4ad1ce128da314e4953e7178f3cc64ccdec6a2f
leejaeryung <jrlii0509@naver.com>
2022-11-11 13:13:37 +0900

    remote Commit 1

--- ssearch_sentinel.py
+++ ssearch_sentinel.py
@@ -28,3 +28,6 @@ if __name__ == '__main__':
         print("검색값을 갖는 원소가 존재하지 않습니다.")
     else:
         print(f'검색값은 x[{idx}]에 있습니다.)
+              
+              
+              print("fetch & pull")
