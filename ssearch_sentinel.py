d844de1d339b41a447f0c6763474a238188036ff
leejaeryung <jrlii0509@naver.com>
2022-11-11 10:45:53 +0900

    my Commit 2

--- ssearch_sentinel.py
+++ ssearch_sentinel.py
@@ -20,3 +20,11 @@ if __name__ == '__main__':
 
     for i in range(num):
         x[i] = int(input(f'x[{i}]: '))
+    ky = int(input('검색할 값을 입력하세요'))
+
+    idx = seq_search(x,ky)
+
+    if idx == -1:
+        print("검색값을 갖는 원소가 존재하지 않습니다.")
+    else:
+        print(f'검색값은 x[{idx}]에 있습니다.)
